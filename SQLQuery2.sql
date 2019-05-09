54def7088f9f0b1feaabbd27807899544daaf306
pegahhdv <49595114+pegahhdv@users.noreply.github.com>
2019-05-09 13:34:33 +0430

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,15 @@
+﻿CREATE DATABASE PEGAH
+USE PEGAH
+CREATE TABLE WORDS
+(
+   USERID int identity primary key,
+   USERName varchar(50),
+   Name varchar(50),
+   PASSWORDD varchar(10),
+   A int,
+   B varchar(50)
+)
+INSERT INTO WORDS( USERName, Name, PASSWORDD) VALUES
+('EDWARD','HH','123');
+
+------
---
+++ SQLQuery3.sql
@@ -0,0 +1,22 @@
+﻿CREATE DATABASE University
+USE University
+CREATE TABLE Students
+(
+   StudentID int identity primary key,
+   FirstName varchar(50),
+   LastName varchar(50),
+   Gender varchar(10),
+   Age int,
+   Address varchar(50)
+)
+INSERT INTO Students(FirstName, LastName, Gender, Age, Address) VALUES
+('Edward', 'Lyons', 'Male', 17, 'Spencer Street'),
+('Jimmie', 'Vargas', 'Male', 18, 'Blue Bay Avenue'),
+('Monica', 'Ward', 'Female', 16, 'Mapple Street'),
+('Joann', 'Jordan', 'Female', 17, 'Spencer Street'),
+('Cheryl', 'Swanson', 'Female', 17, 'Wacky Street'),
+('Clara', 'Webb', 'Female', 18, 'Spooner Street'),
+('Zack', 'Norris', 'Male', 19, 'Blue Bay Avenue'),
+('Randall', 'May', 'Male', 18, 'Golden Street'),
+('Jessica', 'Cole', 'Female', 17, 'Mapple Street'),
+('Oscar', 'Manning', 'Male', 18, 'Mapple Street')
